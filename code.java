6e62ed5a6801f696e0a8e710a6578bcc28bcdbc3
Sushant kumar jha <72967077+sushant135@users.noreply.github.com>
2022-10-16 09:56:03 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,24 @@
+class LinkedList {
+
+  // create an object of Node class
+  // represent the head of the linked list
+  Node head;
+
+  // static inner class
+  static class Node {
+    int value;
+
+    // connect each node to next node
+    Node next;
+
+    Node(int d) {
+      value = d;
+      next = null;
+    }
+  }
+
+  public static void main(String[] args) {
+
+   
+  }
+}
